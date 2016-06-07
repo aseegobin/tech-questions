c572d1b51675f38787c54453aee7f9de9bc7b88c
Avie Seegobin <avieseegobin@gmail.com>
2016-06-07 17:00:27 -0400

    :sparkles:

--- snapchat_1.jsx
+++ snapchat_1.jsx
@@ -1,11 +1,12 @@
 // Given a Binary Tree with the following orientation.
 // Print the word SNAPCHAT so it comes out humanly readable.
 /**
- *       S
- *      / \
- *     A   N
- *    /    /\
- *   P    C  H
- *  /   /
- * T  A
+ *        S
+ *       / \
+ *      A   N
+ *     /    /\
+ *    P    C  H
+ *   /    /
+ *  T    A
+ * 
  */
