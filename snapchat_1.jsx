e884148c57abbba1e3f0528e690dbe0e0c05dd35
Avie Seegobin <avieseegobin@gmail.com>
2016-06-07 16:54:17 -0400

    snapchat interview

---
+++ snapchat_1.jsx
@@ -0,0 +1,11 @@
+// Given a Binary Tree with the following orientation.
+// Print the word SNAPCHAT so it comes out humanly readable.
+/**
+ *        S
+ *      /^^\
+ *     A    N
+ *    /    /^\
+ *   P    C   H
+ *  /   /
+ * T  A
+ */
